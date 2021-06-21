d86f85487ffffc76f1910bac38703ece90ee7b99
Yadier <yadierwww@gmail.com>
2021-06-21 12:14:14 -0500

    ruta adminn

--- src/componets/Navbar.js
+++ src/componets/Navbar.js
@@ -6,22 +6,22 @@ export const Navbar = ({isLoggedIn}) => {
         
         <nav className=" navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#"> 
+    <Link className="navbar-brand" to='/login'> 
     <img src=" https://i.ibb.co/MZtfLzT/logo-block-Buster.png" alt="" height='50px'/>
-   </a>
+   </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/home">Todas</a>
+          <Link className="nav-link active" aria-current="page" to="/home">Todas</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link active" href="#">Más valoradas</a>
+          <Link className="nav-link active" to="/home">Más valoradas</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link active" href="#">Menos valoradas</a>
+          <Link className="nav-link active" to="/home">Menos valoradas</Link>
         </li>
       <li className="nav-item">
         
